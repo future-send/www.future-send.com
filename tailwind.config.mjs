2c61dfb09805e8ad096f7cdaed188c89fbd3b752
Cris Delli-Zotti <crid71@gmail.com>
2025-05-05 13:16:19 -0500

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< Updated upstream
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+=======
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+>>>>>>> Stashed changes
   theme: {
     extend: {
       fontFamily: {
