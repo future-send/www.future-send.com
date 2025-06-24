50edb97ed71bde0254539a339ef662f9d46459ec
Cris Delli-Zotti <crid71@gmail.com>
2025-06-24 09:51:39 -0500

    v1.3.3

--- app/page.jsx
+++ app/page.jsx
@@ -13,10 +13,10 @@ export default function Home() {
       <div className="relative flex items-center justify-center min-h-screen text-center px-4 sm:px-10 md:px-20 lg:px-32 xl:px-60 overflow-hidden bg-neutral-900/80 text-neutral-400">
         <div>
           <h1 className="text-6xl sm:text-8xl md:text-8xl lg:text-8xl xl:text-9xl 2xl:text-[160px] font-bold border-b border-neutral-500 leading-tight tracking-tighter">Cris Delli-Zotti</h1>
-          <h3 className="text-neutral-400 text-lg sm:text-xl md:text-2xl font-light mt-6 sm:mt-8 lg:px-12 xl:px-32">
-            A multidisciplinary professional combining a strong eye for design with <span className="font-bold">Full-Stack development</span> skills. Experienced in crafting user-centered interfaces and building scalable web applications using modern technologies like <span className="font-bold">React</span>, <span className="font-bold">NextJS</span>,
+          <h3 className="text-neutral-400 text-lg sm:text-xl md:text-2xl font-light mt-6 sm:mt-8 lg:px-12 xl:px-36">
+            A multidisciplinary professional combining a strong eye for <span className="font-bold uppercase">UX/UI design</span>  with <span className="font-bold uppercase">Full-Stack development</span> skills. Experienced in crafting user-centered interfaces and building scalable web applications using modern technologies like <span className="font-bold uppercase">React</span>, <span className="font-bold uppercase">NextJS</span>,
             <span> </span>
-            <span className="font-bold">Tailwind CSS</span>, <span className="font-bold">NodeJS</span>, and different <span className="font-bold">Open-source Backend-as-a-Service (BaaS)</span> platform. Passionate about creating intuitive, high-performance digital experiences that merge aesthetics with functionality. 🚀
+            <span className="font-bold uppercase">Tailwind CSS</span>, <span className="font-bold uppercase">NodeJS</span>, and different <span className="font-bold uppercase">Open-source Backend (BaaS)</span> platform. Passionate about creating intuitive, high-performance digital experiences that merge aesthetics with functionality. 🚀
           </h3>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link href="/Portfolio" className="rounded-md bg-neutral-400 px-5 py-2.5 text-xl font-semibold text-neutral-900 hover:bg-neutral-100 hover:text-neutral-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-600 transition duration-500">
